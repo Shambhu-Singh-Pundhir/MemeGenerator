c59101027e708d8bb8ecb4c5839783553ebcaa38
Shambhu-Singh-Pundhir <113406527+Shambhu-Singh-Pundhir@users.noreply.github.com>
2023-02-04 17:09:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Navbar from "./components/Header"
+import Hero from "./components/Hero"
+import Card from "./components/Meme"
+import data from "./memesData"
+
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+/**
+ * Challenge: 
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
